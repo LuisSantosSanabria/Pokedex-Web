fcb104adf250f52537773c07a1da889cfc3d5327
Luis Santos Sanabria <llsssantos1993@gmail.com>
2025-04-30 10:47:23 -0300

    Agregando vistas

--- pokedex-web/DetallePokemon.aspx.cs
+++ pokedex-web/DetallePokemon.aspx.cs
@@ -4,18 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using negocio;
-using dominio;
 
 namespace pokedex_web
 {
-    public partial class WebForm1 : System.Web.UI.Page
+    public partial class WebForm3 : System.Web.UI.Page
     {
-        public List<Pokemon> ListaPokemon { get; set; }
         protected void Page_Load(object sender, EventArgs e)
         {
-            PokemonNegocio negocio = new PokemonNegocio();
-            ListaPokemon = negocio.listarConSp();
+
         }
     }
 }
